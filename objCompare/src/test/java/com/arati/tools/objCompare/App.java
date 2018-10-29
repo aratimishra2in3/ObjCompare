64773a11e0284a7006a45cf8b90345f6348f0923
aratimishra2in3 <42104359+aratimishra2in3@users.noreply.github.com>
2018-10-29 01:05:36 -0700

    Deep object comparator

---
+++ objCompare/src/test/java/com/arati/tools/objCompare/App.java
@@ -0,0 +1,13 @@
+package com.arati.tools.objCompare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
