7a62902b216c3f9bbf8eeffb6786ecedb158357e
aratimishra2in3 <42104359+aratimishra2in3@users.noreply.github.com>
2018-10-29 00:57:55 -0700

    Test Cases

---
+++ Car.java
@@ -0,0 +1,7 @@
+package com.arati.tools.objCompare;
+
+public class Car {
+	public int number_of_wheels;
+	public Wheel wheel;
+
+}
---
+++ Spoke.java
@@ -0,0 +1,6 @@
+package com.arati.tools.objCompare;
+public class Spoke {
+	public String material;
+	public int length;
+
+}
